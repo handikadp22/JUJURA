9395bf0e1994dad7a9934026cb2e6f945365197d
Handika Dwi Prakoso <33805739+handikadp22@users.noreply.github.com>
2023-02-08 20:55:19 +0700

    Add files via upload

--- jujura.php
+++ jujura.php
@@ -1,11 +1,11 @@
 <?Php
 
 //REGISTER
-function curl($url, $data){
+function curl($url, $datas){
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $output = curl_exec($ch); 
     curl_close($ch);      
@@ -13,33 +13,60 @@ function curl($url, $data){
 }
 
 // Data Parameter yang Dikirim oleh cURL
-$data = array("user_name"=>"HandikaDwi",
+$datas = array("user_name"=>"HandikaDwi",
 			  "user_email"=>"Handika@tester.com");
-$send = curl("https://recruitment.jujura.id/api/register",json_encode($data));
+$send = curl("https://recruitment.jujura.id/api/register",json_encode($datas));
 
-echo json_encode(array('respon'=>$send),JSON_UNESCAPED_SLASHES);
 
+// mengubah JSON menjadi array
+$datas = json_decode($send, TRUE);
+
+echo "<pre>";
+print_r($datas);
+echo "</pre>";
 
 //PRODUK
-$url = 'https://recruitment.jujura.id/api/product';
-
-echo httpGet($url);
-
-//print_r(httpGet($url));
-
-function httpGet($url)
-{
-    $ch = curl_init();  
- 
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-//  curl_setopt($ch,CURLOPT_HEADER, false); 
- 
-    $output=curl_exec($ch);
- 
-    curl_close($ch);
+function curly($url){
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $output = curl_exec($ch); 
+    curl_close($ch);      
     return $output;
 }
+
+$send = curly("https://recruitment.jujura.id/api/product");
+
+// mengubah JSON menjadi array
+$data = json_decode($send, TRUE);
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+// $url = 'https://recruitment.jujura.id/api/product';
+
+// echo httpGet($url);
+
+// //print_r(httpGet($url));
+
+// function httpGet($url)
+// {
+//     $ch = curl_init();  
+ 
+//     curl_setopt($ch,CURLOPT_URL,$url);
+//     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+// //  curl_setopt($ch,CURLOPT_HEADER, false); 
+ 
+//     $output=curl_exec($ch);
+ 
+//     curl_close($ch);
+//     return $output;
+//     $profile = json_decode($url,true);
+// echo "<pre>";
+// print_r($profile);
+// echo "</pre>";
+// }
+
 
 // //SALES INSERT
 // function curll($url, $data){
@@ -71,37 +98,4 @@ function httpGet($url)
 // //extract data from the post
 // extract($_POST);
 
-
-
-//set POST variables
-$url = 'https://recruitment.jujura.id/api/sales/insert';
-$fields = array("signature_key "=>"dec1f9a360de6fc4e1dc61b96e173f49",
-		"payment_type "=>"ECHANNEL",
-		"gross_amount"=>1500,
-		"currency "=>"IDR",
-		"items"=>array("item_id"=>"ABC",
-				  "qty"=>2,
-				  "price"=>"1500",
-				  "total"=>'3000')
-		);
-$fields_string = http_build_query($fields);
-
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');
-
-//open connection
-$ch = curl_init();
-
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL, $url);
-curl_setopt($ch,CURLOPT_POST, count($fields));
-curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-//execute post
-$result = curl_exec($ch);
-echo $result;
-
-//close connection
-curl_close($ch);
 ?>
